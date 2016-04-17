3a0a0f28ff792b474d52b1279137b3482d62b948
SyedOsman33 <syed.osmaniye@gmail.com>
2016-04-18 00:09:05 +0500

    RTSP Implementation try Using Sharp-RTSP(repository) library

---
+++ ConsoleApplication1/Implementation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rtsp.Messages;
+using System.Net.Sockets;
+using RtspMulticaster;
+
+
+    namespace RTSPimplementation
+    {
+    class Implementation 
+    {
+        public UdpClient forwardPort = new UdpClient(554);
+        public PortCouple UserPorts = new PortCouple(554);
+
+        private Dictionary<Uri, Forwarder> _listOfForwarder = new Dictionary<Uri, Forwarder>();
+        /* No of forwarding ports available*/
+
+        public List<string> _clientList { get; set; }
+        /*get the ports of clients in session*/
+
+
+    }
+}
